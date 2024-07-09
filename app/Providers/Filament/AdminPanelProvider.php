dd6f12841e38b12616f9c57ed7b2beabdfda66de
Mohamed Ajmal <ajmal@mindster.in>
2024-07-09 14:00:52 +0530

    changes

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -48,7 +48,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->plugins([
                 EnvironmentIndicatorPlugin::make(),
-                \RickDBCN\FilamentEmail\FilamentEmail::make()
+                // \RickDBCN\FilamentEmail\FilamentEmail::make()
             ])
             ->middleware([
                 EncryptCookies::class,
