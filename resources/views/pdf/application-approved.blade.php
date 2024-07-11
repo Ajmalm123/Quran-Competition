c3859ec0ad6c31a6d9775ad41b55de43a0938919
Ajmal M <ajmal@mindster.in>
2024-07-11 08:05:02 +0530

    changes

--- resources/views/pdf/application-approved.blade.php
+++ resources/views/pdf/application-approved.blade.php
@@ -2,18 +2,20 @@
 <html>
 
 <head>
-    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Malayalam:wght@100..900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/fonts.css') }}">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    {{-- <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Malayalam:wght@100..900&display=swap" rel="stylesheet"> --}}
+    {{-- <link rel="stylesheet" href="{{ asset('css/fonts.css') }}"> --}}
     {{-- <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Malayalam:wght@100..900&display=swap" rel="stylesheet"> --}}
     <title>ASLAM HOLY QUR’AN AWARD - Emailer</title>
 
     <style>
-        /* @import url('https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap'); */
-        @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+Malayalam:wght@100..900&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap');
+
+        /* @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+Malayalam:wght@100..900&display=swap'); */
         body {
-            font-family: 'Noto Sans Malayalam', sans-serif;
+            font-family: "Raleway", sans-serif;
             width: 210mm;
             height: 297mm;
             margin: 0;
