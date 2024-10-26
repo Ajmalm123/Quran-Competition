56a592a6fc44ffac3701d41c4b5eae6124ed0faf
Ajmal M <ajmal@mindster.in>
2024-10-26 16:31:46 +0530

    changes

--- app/Http/Controllers/Api/ApplicantController.php
+++ app/Http/Controllers/Api/ApplicantController.php
@@ -14,15 +14,20 @@ class ApplicantController extends Controller
     {
         $perPage = $request->input('per_page', 15);
         $zoneId = $request->input('zone_id');
+        $admitStatus = $request->input('admit_status', 'all');
 
-        $cacheKey = "approved_applicants_page_{$request->input('page', 1)}_perPage_{$perPage}_zoneId_{$zoneId}";
+        $cacheKey = "approved_applicants_page_{$request->input('page', 1)}_perPage_{$perPage}_zoneId_{$zoneId}_admitStatus_{$admitStatus}";
 
-        $applicants = Cache::remember($cacheKey, now()->addMinutes(5), function () use ($perPage, $zoneId) {
+        $applicants = Cache::remember($cacheKey, now()->addMinutes(5), function () use ($perPage, $zoneId, $admitStatus) {
             $query = Application::where('status', 'Approved')
                 ->with('zone:id,name')
                 ->whereHas('zone', function ($subQuery) use ($zoneId) {
                     $subQuery->where('id', $zoneId);
                 });
+
+            if ($admitStatus !== 'all') {
+                $query->where('admit_status', $admitStatus);
+            }
 
             return $query->paginate($perPage);
         });
