e3f8b50f0622a377fea1696556af5932e1ba24ce
Ajmal M <ajmal@mindster.in>
2024-11-03 17:59:59 +0530

    fixes

--- app/Filament/Resources/RankingResource/Pages/ListRankings.php
+++ app/Filament/Resources/RankingResource/Pages/ListRankings.php
@@ -2,16 +2,25 @@
 
 namespace App\Filament\Resources\RankingResource\Pages;
 
-use Filament\Resources\Pages\ListRecords;
 use App\Filament\Resources\RankingResource;
-use Filament\Pages\Concerns\ExposesTableToWidgets;
+use Filament\Resources\Pages\ListRecords;
+use Filament\Actions;
 
 class ListRankings extends ListRecords
 {
-
-    use ExposesTableToWidgets;
-    protected ?string $maxContentWidth = 'full';
-
-
     protected static string $resource = RankingResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            // You can add actions here if needed
+        ];
+    }
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            // You can add widgets here if needed
+        ];
+    }
 }
