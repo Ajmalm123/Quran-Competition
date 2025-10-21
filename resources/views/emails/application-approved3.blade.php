f0aa1b1dfe5ed6d2aff29405d3cca9b3edc38e07
Ajmalm786 <ajmal@mindster.in>
2025-10-21 19:32:40 +0530

    Add new email template for application approval notifications in 'emails.application-approved3.blade.php'. This template includes personalized messages and styling for improved communication with applicants.

---
+++ resources/views/emails/application-approved3.blade.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>ASLAM HOLY QUR'AN AWARD - Emailer</title>
+
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap');
+
+        body {
+            font-family: "Raleway", sans-serif;
+        }
+
+        h1 {
+            font-size: 16px;
+            margin-bottom: 10px;
+            margin-top: 10px;
+        }
+
+        h5 {
+            margin: 0 20px;
+            padding: 15px 20px;
+            background-color: #fff;
+        }
+    </style>
+</head>
+
+<body>
+    <table width="600" height="100" align="center" style="margin-top:20px;">
+        <tbody>
+            <tr>
+                <td width="600" align="center">
+                    <table width="600" style="border: 1px solid #E9EAEC; border-spacing: 0;background-color: #F2F2F2;">
+                        <tbody>
+                            <tr>
+                                <td style="padding:0;">
+                                    <table width="600" cellpadding="0" cellspacing="0" border="0"
+                                        background="https://aslamquranaward.com/images/header-bg.png"
+                                        style="background-color: #f2f2f2; background-repeat: no-repeat; background-size: cover; border-spacing: 0; margin:40px 18px 0 18px; padding:20px;">
+                                        <tr>
+                                            <td width="300" style="padding-left:16px;">
+                                                <h1 style="font-size:14px;color:#fff;margin-bottom:0;">AP Aslam Holy Qur'an</h1>
+                                                <h1 style="font-size:14px;color:#fff;margin-top:5px;">Award 2025</h1>
+                                            </td>
+                                            <td width="300" style="padding-left:20px;text-align:right;">
+                                                <img style="width:15em" src="{{ $message->embed(public_path('images/logo-email.png')) }}">
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="padding:0px 20px;">
+                                    <h5 style="font-size: 14px; margin-top: 0px;margin-bottom: 0px;font-weight: 600;color:#000000;padding-top:50px;">
+                                        لسَّلامُ عَلَيْكُم ورَحْمَةُ اللهِ وَبَرَكاتُهُ
+                                    </h5>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="padding:0px 20px;">
+                                    <h5 style="font-size: 14px; margin-top: 0px;margin-bottom: 0px;font-weight: 700;color:#000000;">
+                                        <strong>പ്രിയപ്പെട്ട {{ $mailData['application']->gender == 'male' ? 'ഹാഫിസ്' : 'ഹാഫിസ' }} {{ $mailData['application']->full_name ?? 'Applicant' }},</strong>
+                                    </h5>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="padding:0px 20px;">
+                                    <h5 style="line-height:22px;font-size: 14px; margin-top: 0px;margin-bottom: 0px;font-weight: 400;color:#000000;">
+                                        എ പി അസ്‌ലം ഹോളി അവാർഡ് 2025 മത്സരത്തിൽ പങ്കെടുക്കുന്നതിനായി താങ്കൾ സമർപ്പിച്ച അപേക്ഷ പരിശോധിക്കുകയും അംഗീകരിക്കുകയും ചെയ്തതായി അറിയിക്കുന്നതിൽ ഞങ്ങൾക്ക് സന്തോഷമുണ്ട്. അഭിനന്ദനങ്ങൾ!
+                                    </h5>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="padding:0px 20px;">
+                                    <h5 style="line-height:22px;font-size: 14px; margin-top: 0px;margin-bottom: 0px;font-weight: 400;color:#000000;">
+                                        താങ്കൾ മനസ്സിലാക്കിയത് പോലെ വിശുദ്ധ ഖുർആൻ പരിപൂർണ്ണമായ മനഃപാഠവും തജ് വീദ് നിയമങ്ങൾ അനുസരിച്ചുള്ള പാരായണവുമായിരിക്കും മത്സരത്തിന്റെ ഭാഗമായി പരിശോധിക്കുക. അതോടൊപ്പം 22 വയസ്സിന് താഴെയുള്ള ആൺകുട്ടികളുടെ വിഭാഗത്തിൽ ആദ്യ അവസാന അഞ്ചു ജുസ്ഉകൾ ഒഴികെയുള്ള 20 ജുസ്ഇന്റെയും, 22 വയസ്സിന് താഴെയുള്ള പെൺകുട്ടികളുടെ വിഭാഗം മത്സരങ്ങളിൽ ഖുർആനിലെ ആദ്യ അവസാന അഞ്ചു വീതം ജുസ്ഉകളുടെയും ആശയസംബന്ധമായ പരിശോധനയും ഉണ്ടായിരിക്കും. 13 വയസ്സിന് താഴെയുള്ള കുട്ടികളുടെ വിഭാഗത്തിൽ ഹിഫ്സ് മാത്രമായിരിക്കും പരിശോധിക്കുക.
+                                    </h5>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="padding:0px 20px;">
+                                    <h5 style="line-height:22px;font-size: 14px; margin-top: 0px;margin-bottom: 0px;font-weight: 400;color:#000000;">
+                                        നവംബർ അവസാനവാരത്തിൽ നടക്കുന്ന സ്ക്രീനിങ് മത്സരങ്ങളുടെ സ്ഥലവും തീയതിയും സമയവും ഇമെയിൽ സന്ദേശമായി താങ്കൾക്ക് പിന്നീട് ലഭിക്കുന്നതായിരിക്കും.
+                                    </h5>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="padding:0px 20px;">
+                                    <h5 style="line-height:22px;font-size: 14px; margin-top: 0px;margin-bottom: 0px;font-weight: 400;color:#000000;">
+                                        മത്സരത്തിനു വേണ്ടി നന്നായി തയ്യാറെടുക്കുകയും പരിശ്രമിക്കുകയും ചെയ്യണമെന്ന് അറിയിക്കുന്നു.
+                                    </h5>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="padding:0px 20px;">
+                                    <h5 style="line-height:22px;font-size: 14px; margin-top: 0px;margin-bottom: 0px;font-weight: 400;color:#000000;">
+                                        കൂടുതൽ വിവരങ്ങൾക്ക് ഞങ്ങളെ ബന്ധപ്പെടാവുന്നതാണ്.
+                                    </h5>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="padding:0px 20px;">
+                                    <h5 style="padding-bottom:8px;font-size: 14px; margin-top: 0px;margin-bottom: 0px;font-weight: 400;color:#000000;">
+                                        വിശ്വസ്തതയോടെ,
+                                    </h5>
+                                    <h5 style="font-size: 14px; margin-top: 0px;padding-top:0;padding-bottom:10px;margin-bottom: 0px;font-weight: 400;color:#000000;">
+                                        കോ ഓർഡിനേറ്റർ<br>
+                                        എ പി അസ്‌ലം ഹോളി ഖുർആൻ അവാർഡ് കമ്മിറ്റി
+                                    </h5>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="padding:0px 20px;">
+                                    <h5 style="font-size: 14px; margin-top: 0px;margin-bottom: 0px;font-weight: 400;color:#000000;">
+                                        9846310383
+                                    </h5>
+                                    <h5 style="font-size: 14px; margin-top: 0px;margin-bottom: 0px;font-weight: 400;color:#000000;">
+                                        info@aslamquranaward.com
+                                    </h5>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="text-align:center;padding-top:40px;">
+                                    <img src="{{ $message->embed(public_path('images/footer-mail-logo.png')) }}">
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="padding:0px 20px;">
+                                    <a href="https://aslamquranaward.com/" target="_blank">
+                                        <h5 style="color:#000000;font-size: 8px;text-align:center; margin-top: 10px;margin-bottom: 0px;font-weight: 400;padding-bottom:0; background-color: transparent;">
+                                            Copyright © 2025 Aslam Quran Award, All rights reserved.
+                                        </h5>
+                                    </a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="padding:0px 20px;">
+                                    <h5 style="padding-top:16px;font-size: 8px;color:#000000;text-align:center; margin-top: 0px;margin-bottom: 0px;font-weight: 400;color:#000000;background-color: transparent;">
+                                        Follow us on
+                                    </h5>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="padding:0px 20px 30px 20px;text-align:center;">
+                                    <a style="text-decoration: none;" href="https://www.instagram.com/apaslamquranaward/?utm_source=qr&igsh=YjZ4OXNieTRocXdy" target="_blank">
+                                        <img src="{{ $message->embed(public_path('images/insta 2.png')) }}">
+                                    </a>
+                                    <a style="text-decoration: none;" href="https://youtube.com/@apaslamquranaward?si=hrE99ldkB2tm7fqP" target="_blank">
+                                        <img src="{{ $message->embed(public_path('images/youtube 2.png')) }}">
+                                    </a>
+                                    <a style="text-decoration: none;" href="https://www.facebook.com/apaslamquranaward" target="_blank">
+                                        <img src="{{ $message->embed(public_path('images/facebook 3.png')) }}">
+                                    </a>
+                                    <a style="text-decoration: none;" href="https://www.x.com/apaslamquranaward" target="_blank">
+                                        <img src="{{ $message->embed(public_path('images/twitter 3.png')) }}">
+                                    </a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+
+</html>
